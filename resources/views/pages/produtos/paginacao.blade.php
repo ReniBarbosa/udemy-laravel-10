7e91546eb29fc9bbe1921ca2a7fe236b08486769
Renilson <renilsonbarbosa123@gmail.com>
2023-11-01 17:51:12 -0300

    seeder,migrations e controlers

---
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -0,0 +1,14 @@
+@extends('index')
+
+@section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Produtos</h1>
+</div>
+<div>
+    <form action="" method="GET">
+        <input type="text" name="pesquisar" placeholder="Digite o nome"/>  
+        <button class=" btn btn-danger">Pesquisar</button> 
+        <a href="" type="button" class="btn btn-success float-end">Incluir</a>
+    </form>
+</div>
+@endsection
