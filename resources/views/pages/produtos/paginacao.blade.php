708a5652dfd1098d88c4aed7118b71a38f38bc59
Renilson <renilsonbarbosa123@gmail.com>
2023-11-02 15:52:48 -0300

    up

--- resources/views/pages/produtos/paginacao.blade.php
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -7,8 +7,33 @@
 <div>
     <form action="" method="GET">
         <input type="text" name="pesquisar" placeholder="Digite o nome"/>  
-        <button class=" btn btn-danger">Pesquisar</button> 
+        <button class=" btn btn-primary">Pesquisar</button> 
         <a href="" type="button" class="btn btn-success float-end">Incluir</a>
     </form>
+    <div class="table-responsive small mt-4">
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th >Nome</th>
+                    <th >Valor</th>
+                    <th >Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($findProdutos as $produto)
+                <tr>
+                    <td>{{$produto->nome}}</td>
+                    <td>{{'R$'.''.number_format($produto->valor, 2, ',','.')}}</td>
+                    <td>
+                        <a href="" class="btn btn-primary btn-sm">Editar</a>
+                        <a href="" class="btn btn-danger btn-sm">Excluir</a>
+                    </td>                
+                </tr>                    
+                @endforeach
+            </tbody>
+        </table>
+    </div> 
 </div>
+<!-- Listagem de produtos-->
+
 @endsection
